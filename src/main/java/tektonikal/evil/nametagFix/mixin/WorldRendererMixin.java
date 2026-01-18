fbb5269606714d2ee6e6c8db5096614cc5ffdd8e
Nightly <pixel@nightly.eu.org>
2026-01-19 00:29:40 +0200

    downgrade to 1.16-1.16.4

--- src/main/java/tektonikal/evil/nametagFix/mixin/WorldRendererMixin.java
+++ src/main/java/tektonikal/evil/nametagFix/mixin/WorldRendererMixin.java
@@ -8,7 +8,7 @@ import net.minecraft.client.render.WorldRenderer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
-import org.joml.Matrix4f;
+import net.minecraft.util.math.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,7 +19,7 @@ import static tektonikal.evil.nametagFix.client.NametagFixClient.yeah;
 
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
-    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;renderChunkDebugInfo(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/Camera;)V"))
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;renderChunkDebugInfo(Lnet/minecraft/client/render/Camera;)V"))
     private void ough(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
         isItTheLastWorldRenderStage = true;
         for (Entity entity : yeah) {
@@ -28,7 +28,7 @@ public class WorldRendererMixin {
             double e = MathHelper.lerp(tickDelta, entity.lastRenderY, entity.getY());
             double f = MathHelper.lerp(tickDelta, entity.lastRenderZ, entity.getZ());
             matrices.translate(d - camera.getPos().x, e - camera.getPos().y, f - camera.getPos().z);
-            ((Yeah) MinecraftClient.getInstance().getEntityRenderDispatcher().getRenderer(entity)).yeah(entity, entity.getDisplayName(), matrices, MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers(), MinecraftClient.getInstance().getEntityRenderDispatcher().getLight(entity, tickDelta));
+            ((Yeah) MinecraftClient.getInstance().getEntityRenderManager().getRenderer(entity)).yeah(entity, entity.getDisplayName(), matrices, MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers(), MinecraftClient.getInstance().getEntityRenderManager().getLight(entity, tickDelta));
             matrices.pop();
         }
         yeah.clear();
