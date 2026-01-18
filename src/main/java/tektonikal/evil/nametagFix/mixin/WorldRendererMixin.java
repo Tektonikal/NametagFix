8319ff24b1f6f6f0604245023253f74c4a09a5a8
Nightly <pixel@nightly.eu.org>
2026-01-19 01:06:26 +0200

    upgrade to 1.20.5-1.21.1

--- src/main/java/tektonikal/evil/nametagFix/mixin/WorldRendererMixin.java
+++ src/main/java/tektonikal/evil/nametagFix/mixin/WorldRendererMixin.java
@@ -1,10 +1,8 @@
 package tektonikal.evil.nametagFix.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.GameRenderer;
-import net.minecraft.client.render.LightmapTextureManager;
-import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
@@ -13,6 +11,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import static tektonikal.evil.nametagFix.client.NametagFixClient.isItTheLastWorldRenderStage;
 import static tektonikal.evil.nametagFix.client.NametagFixClient.yeah;
@@ -20,16 +19,16 @@ import static tektonikal.evil.nametagFix.client.NametagFixClient.yeah;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
     @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/WorldRenderer;renderChunkDebugInfo(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;Lnet/minecraft/client/render/Camera;)V"))
-    private void ough(MatrixStack matrices, float tickDelta, long limitTime, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f positionMatrix, CallbackInfo ci) {
+    private void ough(RenderTickCounter tickCounter, boolean renderBlockOutline, Camera camera, GameRenderer gameRenderer, LightmapTextureManager lightmapTextureManager, Matrix4f matrix4f, Matrix4f matrix4f2, CallbackInfo ci, @Local MatrixStack matrixStack) {
         isItTheLastWorldRenderStage = true;
         for (Entity entity : yeah) {
-            matrices.push();
-            double d = MathHelper.lerp(tickDelta, entity.lastRenderX, entity.getX());
-            double e = MathHelper.lerp(tickDelta, entity.lastRenderY, entity.getY());
-            double f = MathHelper.lerp(tickDelta, entity.lastRenderZ, entity.getZ());
-            matrices.translate(d - camera.getPos().x, e - camera.getPos().y, f - camera.getPos().z);
-            ((Yeah) MinecraftClient.getInstance().getEntityRenderDispatcher().getRenderer(entity)).yeah(entity, entity.getDisplayName(), matrices, MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers(), MinecraftClient.getInstance().getEntityRenderDispatcher().getLight(entity, tickDelta));
-            matrices.pop();
+            matrixStack.push();
+            double d = MathHelper.lerp(tickCounter.getTickDelta(true), entity.lastRenderX, entity.getX());
+            double e = MathHelper.lerp(tickCounter.getTickDelta(true), entity.lastRenderY, entity.getY());
+            double f = MathHelper.lerp(tickCounter.getTickDelta(true), entity.lastRenderZ, entity.getZ());
+            matrixStack.translate(d - camera.getPos().x, e - camera.getPos().y, f - camera.getPos().z);
+            ((Yeah) MinecraftClient.getInstance().getEntityRenderDispatcher().getRenderer(entity)).yeah(entity, entity.getDisplayName(), matrixStack, MinecraftClient.getInstance().getBufferBuilders().getEntityVertexConsumers(), MinecraftClient.getInstance().getEntityRenderDispatcher().getLight(entity, tickCounter.getTickDelta(true)), tickCounter.getTickDelta(true));
+            matrixStack.pop();
         }
         yeah.clear();
         isItTheLastWorldRenderStage = false;
